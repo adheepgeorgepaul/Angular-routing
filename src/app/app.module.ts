e183e1515bbc31cb5716aeac3dc36f0d8df775d2
adheepgeorgepaul <adheepgeorgepaul@gmail.com>
2020-10-28 15:54:50 +0530

    126.Registering routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
@@ -10,7 +11,14 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { UserComponent } from './users/user/user.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const appRoutes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'users', component: UsersComponent },
+{ path: 'servers', component: ServersComponent },
+];
 
 @NgModule({
   declarations: [
@@ -24,7 +32,8 @@ import { ServersService } from './servers/servers.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
