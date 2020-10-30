257eb344411f68829458e6519f5893163e4ce247
adheepgeorgepaul <adheepgeorgepaul@gmail.com>
2020-10-30 10:34:54 +0530

    149.Controlling Navigation with CanDeactivate guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth-guard.service';
 
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -10,17 +9,30 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'users', component: UsersComponent, children: [
+
+    { path: 'users', component: UsersComponent, 
+      children: [
       { path: ':id/:name', component: UserComponent }
-    ]},
-    { path: 'servers', canActivate:[AuthGuard], component: ServersComponent, children: [
-      { path: ':id/edit', component: EditServerComponent },
+      ]
+    },
+
+    { path: 'servers',
+      // canActivate:[AuthGuard],
+      canActivateChild: [AuthGuard], 
+      component: ServersComponent, 
+      children: [
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]},
       { path: ':id', component: ServerComponent }
-    ] },
+      ] 
+    },
     
     { path: 'not-found', component: PageNotFoundComponent},
+
     { path: '**', redirectTo: '/not-found'}
     
     ];
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -19,10 +19,12 @@ export class AuthService
     login()
     {
         this.loggedIn = true;
+        console.log(`user logged in`);
     }
 
     logout()
     {
         this.loggedIn = false;
+        console.log(`user logged out`);
     }
 }
